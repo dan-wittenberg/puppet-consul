f4cdc61dea5da9fc13ac4c10de38167e05691545
Daniel Wittenberg <dan.wittenberg@thinkahead.com>
2019-01-23 03:08:56 -0600

    license update

--- manifests/license.pp
+++ manifests/license.pp
@@ -9,20 +9,21 @@
 #   include consul::license
 class consul::license {
 
-  file { "${consul::config_dir}/consul_enterprise.hclic":
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0400',
-    content => $consul::enterprise_license,
-  }
+  if($consul::enterprise_license) {
+    file { "${consul::config_dir}/consul_enterprise.hclic":
+      ensure  => file,
+      owner   => 'consul',
+      group   => 'consul',
+      mode    => '0400',
+      content => $consul::enterprise_license,
+    }
 
-  exec { 'update-consul-license':
-    path      => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
-    command   => "consul license put @${consul::config_dir}/consul_enterprise.hclic",
-    onlyif    => 'consul license get |grep "License ID: temporary"',
-    logoutput => true,
-    require   => File["${consul::config_dir}/consul_enterprise.hclic"],
+    exec { 'update-consul-license':
+      path      => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+      command   => "consul license put @${consul::config_dir}/consul_enterprise.hclic",
+      onlyif    => 'consul license get |grep "License ID: temporary"',
+      logoutput => true,
+      require   => File["${consul::config_dir}/consul_enterprise.hclic"],
+    }
   }
-
 }
